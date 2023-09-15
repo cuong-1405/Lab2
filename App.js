a09807c962c7f10fac6351c5014a36ac9f122693
chekhucbach <75738760+cuong-1405@users.noreply.github.com>
2023-09-15 21:51:05 +0700

    lab2

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, SafeAreaView } from "react-native";
+import MyTextComp from "./MyTextComp";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { Component } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class HomePage extends Component {
+  state = {
+    myState: "Trạng thái 1",
+  };
+  updateText = () => {
+    if (this.state.myState == "Trạng thái 1") {
+      this.setState({ myState: "Trạng thái 2" });
+    } else {
+      this.setState({ myState: "Trạng thái 1" });
+    }
+  };
+
+  render() {
+    return (
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text style={styles.heading}>Tiêu đề</Text>
+          <Text style={styles.text}>Nguyễn Văn Cường</Text>
+          <Text style={styles.subTitle}>{this.state.myState}</Text>
+
+          <Button onPress={this.updateText} title="Click me!" color="#841584" />
+
+          <MyTextComp noidungText="Code bai 3" />
+
+          <MyTextComp noidungText="Code bai 4" />
+
+          <View style={styles.horizontalView}>
+            {/* <View style={[styles.redbox, styles.blackbox]} /> */}
+            <View style={styles.redbox} />
+            <View style={styles.bluebox} />
+            <View style={styles.blackbox} />
+          </View>
+
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
+  horizontalView: {
+    flexDirection: "row",
+    marginTop: 20,
+  },
   container: {
+    padding: 10,
+    height: "100%",
+    flexDirection: "column",
+    backgroundColor: "cornsilk",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  redbox: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: 100,
+    backgroundColor: "red",
+  },
+  bluebox: {
+    flex: 2,
+    height: 100,
+    backgroundColor: "green",
+  },
+  blackbox: {
+    flex: 1,
+    height: 100,
+    backgroundColor: "black",
+  },
+  heading: {
+    color: "blueviolet",
+    fontSize: 60,
+  },
+  text: {
+    fontSize: 40,
+  },
+  subTitle: {
+    fontSize: 30,
+    color: "#dd0000",
   },
 });
+export default HomePage;
